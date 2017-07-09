129e89a696bb4d75d9eb3918c9b072fe50ac65d0
Yuri Stecenko <sy.soft.work@gmail.com>
2017-07-09 11:11:13 +0000

    before back

--- sites/all/modules/sy_commerce_add/select_city.js
+++ sites/all/modules/sy_commerce_add/select_city.js
@@ -2,9 +2,8 @@
 
     Drupal.behaviors.sy_commerce_add_block_select_city = {
         attach: function (context, settings) {
-            console.log("s");
 
-            $('#block-sy-commerce-add-select-city-new select').change(function() {
+            $('.block-sy-commerce-add select').change(function() {
                 // set the window's location property to the value of the option the user has selected
                 window.location = $(this).val();
             });
