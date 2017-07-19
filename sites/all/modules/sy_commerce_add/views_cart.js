893e6f7ef60a37d71dae75751102192e12177b99
Oleg Kreminsky <seorusus@gmail.com>
2017-07-19 09:03:38 +0000

    bugs fixing

--- sites/all/modules/sy_commerce_add/views_cart.js
+++ sites/all/modules/sy_commerce_add/views_cart.js
@@ -2,14 +2,14 @@
     Drupal.behaviors.yourName = {
         attach : function(context, settings) {
 
-
+/*
             $( 'div.view-id-taxonomy_term .views-row select[name="product_id"][class!="refreshed"]' ).each(function( index ) {
                 console.log( index + ": " + $( this ).attr('class') );
                 $( this ).parent().addClass("refreshed");
-                $( this ).addClass("refreshed"); 
+                $( this ).addClass("refreshed");
                // $( this ).change();
             });
-
+*/
 
         }
 
